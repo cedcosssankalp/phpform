0053a5ea0b89a97207dafa65ed81f2b274406c12
sankalp <sankalpdixit@cedcoss.com>
2022-02-15 15:34:10 +0530

    program3

--- src/index.php
+++ src/index.php
@@ -1,14 +1,47 @@
-<!DOCTYPE HTML>
 <?php
 
+if(isset($_POST['calculate'])){
+
+$length=$_POST['num1'];
+$width=$_POST['num2'];
+$area;
+$peri;
+
+$area=$length*$width;
+$perimeter=2*($length+$width);
+
+
+}
 
 
 ?>
-<html>  
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>area of rectangle</title>
+</head>
 <body>
-    <form action="php" method="post">
-        name:<input type="text" name="name"><br>
-        number:<input type="numbers" name="numbers"><br>
-        <input type="submit";>
+
+<form action="index.php" method="post">
+ 
+length of rectangle:<input type="text" name="num1"><br><br>
+width of rectangle:<input type="text" name="num2"><br><br>
+
+<input type="submit" name="calculate" value="calculate area and perimeter"><br><br>
+
+
+<p>
+    area is <?php
+   echo $area;
+    ?> sq. mtr <br><br>
+    perimeter is  <?php
+  echo  $perimeter;
+   ?> mtr
+</p>
+
+</form>
+  
 </body>
 </html>
