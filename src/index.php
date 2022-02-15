b20eea3d6941fc1996d6e2ec2f59feb4cc776e3b
sankalp <sankalpdixit@cedcoss.com>
2022-02-15 12:45:31 +0530

    main

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<?php
+
+
+
+?>
+<html>  
+<body>
+    <form action="php" method="post">
+        name:<input type="text" name="name"><br>
+        number:<input type="numbers" name="numbers"><br>
+        <input type="submit";>
+</body>
+</html>
