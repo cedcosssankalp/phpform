9f8dce04c5f61ea4a681c6ef66d4fc5a696771ae
sankalp <sankalpdixit@cedcoss.com>
2022-02-15 14:57:18 +0530

    program2

--- src/index.php
+++ src/index.php
@@ -1,14 +1,42 @@
 <!DOCTYPE HTML>
 <?php
+$result='';
+if(isset($_POST)){
+$n=$_POST['sa'];
+$num1=$_POST['num1'];
+$num2=$_POST['num2'];
+}
+switch($n){
+    case '+':
+        $result=$num1+$num2;
+        break;
 
+    case '-':
+        $result=$num1-$num2;
+        break;
 
+    case '*':
+         $result=$num1*$num2;
+        break; 
+            
+    case '/':
+        $result=$num1/$num2;
+        break;       
 
+}
 ?>
 <html>  
 <body>
-    <form action="php" method="post">
-        name:<input type="text" name="name"><br>
-        number:<input type="numbers" name="numbers"><br>
-        <input type="submit";>
+    <div id="wrapper">
+    <form action="index.php" method="post">
+        Number1:<input type="text" name="num1" value="<?php echo "$num1"?>"><br>
+        Number2:<input type="text" name="num2" value="<?php echo "$num2"?>"><br>
+        results:<input type="text" name="calculate" value="<?php echo "$result"?>"><br>
+        <input type="submit" value="+" name="sa">
+        <input type="submit" value="-" name="sa">
+        <input type="submit" value="*" name="sa">
+        <input type="submit" value="/" name="sa">
+</form>
+</div>
 </body>
 </html>
